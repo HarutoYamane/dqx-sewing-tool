bf0c00c4d63434fd67444bebe0f3778b516398a4
Yamane Haruto <haru.momo.coco.happy@gmail.com>
2025-08-30 17:07:06 +0900

    グローバルメニューのデータ型の作成

---
+++ src/types/workspace.ts
@@ -0,0 +1,17 @@
+export interface Latest {
+  id: number;
+  name: string;
+  imageUrl: string;
+}
+
+export interface Favorite {
+  id: number;
+  name: string;
+  imageUrl: string;
+}
+
+export interface MyProfile {
+  name: string;
+  email: string;
+  imageUrl: string;
+}
