4753ca246a1440c0f8a871667f043f7a72a8c9db
Yamane Haruto <haru.momo.coco.happy@gmail.com>
2025-09-04 18:25:43 +0900

    型情報を記載（後々変更）

--- src/types/workspace.ts
+++ src/types/workspace.ts
@@ -1,5 +1,14 @@
 import { LucideIcon } from 'lucide-react';
 
+// あとでこの型に合わせる
+// "id": "cmf534vvd00068omnw053bbmx",
+//   "name": "テストユーザー1",
+//   "email": "aaaaaa@exaple.com",
+//   "imageUrl": null,
+//   "authId": "bc4e31bb-a434-4d63-9503-7c06dbdfd56d",
+//   "role": "USER",
+//   "createdAt": "2025-09-04T07:29:02.593Z",
+//   "updatedAt": "2025-09-04T07:29:02.593Z"
 export interface UserProfile {
   id: number;
   name: string;
