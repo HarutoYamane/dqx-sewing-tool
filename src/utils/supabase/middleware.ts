97c88e29b4dec4554fd7a16590501bccedd35f6a
Yamane Haruto <haru.momo.coco.happy@gmail.com>
2025-09-05 18:49:13 +0900

    未認証の場合にアクセスできるサイトを適切に設定（ログイン・ランディング・サインアップページ）

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -37,7 +37,13 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (!user && !request.nextUrl.pathname.startsWith('/login') && !request.nextUrl.pathname.startsWith('/auth')) {
+  if (
+    !user &&
+    !request.nextUrl.pathname.startsWith('/login') &&
+    !request.nextUrl.pathname.startsWith('/auth') &&
+    !request.nextUrl.pathname.startsWith('/signup') &&
+    request.nextUrl.pathname !== '/'
+  ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
     url.pathname = '/login';
