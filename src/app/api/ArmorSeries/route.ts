20e11a23a1052045e08bfa1942bb330ffc6b1355
Yamane Haruto <haru.momo.coco.happy@gmail.com>
2025-09-05 18:57:42 +0900

    防具シリーズ毎に取得するAPIを作成（最新・ページ毎に取得）

---
+++ src/app/api/ArmorSeries/route.ts
@@ -0,0 +1,56 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { prisma } from '@/lib/prisma';
+import { MaxLv } from '@/data/workspace';
+
+export async function GET(request: NextRequest) {
+  const latest = request.nextUrl.searchParams.get('latest');
+  const page = request.nextUrl.searchParams.get('page');
+  const skipTotal = request.nextUrl.searchParams.get('skipTotal');
+
+  if (latest) {
+    //最新シリーズのみ取得(latest=trueが渡される)
+    const latestSeries = await prisma.armorSeries.findMany({
+      where: { lv: MaxLv },
+      include: {
+        armors: {
+          orderBy: [
+            { parts: 'asc' }, // 頭・体上・体下・腕・足の順番
+          ],
+        },
+      }, //シリーズの防具を取得
+    });
+    return NextResponse.json(latestSeries);
+  }
+  if (page) {
+    // ページネーション(1ページ20件分まで取得)
+    const pageNumber = parseInt(request.nextUrl.searchParams.get('page') || '1');
+
+    const armorSerieses = await prisma.armorSeries.findMany({
+      orderBy: { lv: 'desc' },
+      include: {
+        armors: {
+          orderBy: [
+            { parts: 'asc' }, // 頭・体上・体下・腕・足の順番
+          ],
+        },
+      },
+      take: 20,
+      skip: (pageNumber - 1) * 20,
+    });
+
+    // skipTotalがtrueの場合はtotalを取得しない
+    if (skipTotal === 'true') {
+      return NextResponse.json({
+        data: armorSerieses,
+      });
+    }
+
+    // 初回のみtotalを取得
+    const total = await prisma.armorSeries.count();
+
+    return NextResponse.json({
+      data: armorSerieses,
+      total,
+    });
+  }
+}
