d16281aa88948821ef95bfd48fbb99757f643709
Yamane Haruto <haru.momo.coco.happy@gmail.com>
2025-09-05 22:05:27 +0900

    裁縫ツールページをデータベースから取得に変更（コンポーネント毎にデータ取得）

--- src/app/workspace/channel/[channelId]/page.tsx
+++ src/app/workspace/channel/[channelId]/page.tsx
@@ -3,32 +3,28 @@
 
 // Next.js
 import { useParams, notFound } from 'next/navigation';
-// コンポーネント
+// shadcn/ui
+import { Separator } from '@/components/ui/separator';
+// 自作コンポーネント
 import ChannelHeader from '@/components/channel/channelHeader';
 import SewingArea from '@/components/channel/sewingArea';
 import SewingValueArea from '@/components/channel/sewingValueArea';
 import ChannelFooter from '@/components/channel/channelFooter';
-// データ
-import { getChannel } from '@/data/armor';
-import { Separator } from '@/components/ui/separator';
 
 export default function ArmorPage() {
   // URL のパスからチャンネル ID を取得
   const { channelId } = useParams<{ channelId: string }>(); //動的なchannelIdを文字列で取得
   const channelIdNumber = parseInt(channelId, 10); //channelIdを数値に変換
-  if (isNaN(channelIdNumber)) return notFound(); //channelIdが数値でない場合は404エラー
-
-  const armor = getChannel(channelIdNumber); //channelId(armorId)に対応する防具情報を取得
-  if (!armor) return notFound(); //channelId(armorId)に対応するチャンネルがない場合は404エラー
+  if (isNaN(channelIdNumber)) return notFound();
 
   return (
     <div className="flex flex-col h-full">
       <div className="sticky top-0 bg-background z-50 border-b">
-        <ChannelHeader armor={armor} />
+        <ChannelHeader channelId={channelIdNumber} />
       </div>
       <div className="flex flex-row gap-4 flex-1 overflow-hidden">
         <div className="flex-1 overflow-auto">
-          <SewingArea armor={armor} />
+          <SewingArea channelId={channelIdNumber} />
         </div>
         <Separator orientation="vertical" className="h-full" />
         <div className="flex-1 overflow-auto">
@@ -36,7 +32,7 @@ export default function ArmorPage() {
         </div>
       </div>
       <div className="h-14 border-t bg-background">
-        <ChannelFooter />
+        <ChannelFooter channelId={channelIdNumber} />
       </div>
     </div>
   );
