61b032ef286bee4cf2e43f402638f2ee501e3f43
Yamane Haruto <haru.momo.coco.happy@gmail.com>
2025-09-05 22:04:36 +0900

    不要API削除

--- src/app/api/Armor/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-import { prisma } from '@/lib/prisma';
-
-export async function GET() {
-  const armors = await prisma.armor.findMany();
-  return NextResponse.json(armors);
-}
