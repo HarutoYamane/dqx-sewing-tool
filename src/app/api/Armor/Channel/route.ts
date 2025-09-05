82d9b144f42e48369524a89cb6df64465d5a23af
Yamane Haruto <haru.momo.coco.happy@gmail.com>
2025-09-05 22:02:10 +0900

    チャンネル毎の防具情報を取得するAPIを作成

---
+++ src/app/api/Armor/Channel/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { prisma } from '@/lib/prisma';
+
+/**
+ * [GET] /api/Armor: ページ毎の防具の詳細情報を取得
+ */
+export async function GET(request: NextRequest) {
+  const ChannelId = request.nextUrl.searchParams.get('channelId');
+
+  if (!ChannelId) {
+    return NextResponse.json({ error: 'channelIdが指定されていません' }, { status: 400 });
+  }
+
+  const armor = await prisma.armor.findUnique({
+    where: {
+      id: parseInt(ChannelId),
+    },
+    include: {
+      sewing: {
+        select: {
+          clothType: true,
+        },
+      },
+    },
+  });
+  return NextResponse.json(armor);
+}
