7ea5b94d935fc79d95b3fbe3964db752fb3cfa3e
Yamane Haruto <haru.momo.coco.happy@gmail.com>
2025-09-05 18:49:55 +0900

    データベースに合わせて型を調整

--- src/types/armor.ts
+++ src/types/armor.ts
@@ -1,23 +1,54 @@
+type ArmorParts = 'HEAD' | 'BODY_UPPER' | 'BODY_LOWER' | 'ARMS' | 'LEGS';
 export interface Armor {
   id: number;
   name: string;
   parts: ArmorParts;
-  lv: number;
-  series: ArmorSeries['name'];
-  Type: ClothType;
-  strength: Strengths[]; //強さのローテーション数はランダムなので要素数は指定しない
-  settingValue: [number, number, number, number, number, number, number, number, number]; // 設定値1から9まで
+  armorSeriesId: number;
   imageUrl: string;
+  lv: number;
 }
-
-type ArmorParts = 'head' | 'bodyUpper' | 'bodyLower' | 'arms' | 'legs';
-type ClothType = '再生布' | '虹布' | '会心布' | '倍半布' | '通常'; //初期装備では布特性の無い装備もある
-type Strengths = '弱い' | '普通' | '強い' | '最強' | '？';
 
 export interface ArmorSeries {
   id: number; //シリーズのID
   name: string; //シリーズの名前
   lv: number; //シリーズのレベル
   imageUrl: string; //シリーズの画像URL（体上装備）
-  latest: boolean; //最新装備かどうか
+  armors?: Armor[]; // 関連する防具（オプショナル）
+}
+
+type ClothType = 'REBIRTH' | 'RAINBOW' | 'HEART' | 'NORMAL'; //初期装備では布特性の無い装備もある
+type Strengths = 'WEAK' | 'NORMAL' | 'STRONGER' | 'STRONGEST' | 'UNKNOWN';
+export interface Swing {
+  id: number;
+  armorId: number;
+  clothType: ClothType; //布特性(再生・虹・会心・通常)
+  strength: Strengths[]; //強さローテーション
+  settingValue: number[]; //防具毎の設定値
+}
+
+export interface Favorite {
+  id: number;
+  userId: string;
+  armorId: number;
+  createdAt: Date;
+  armor: {
+    id: number;
+    name: string;
+    imageUrl: string;
+  };
+}
+
+export interface Result {
+  //裁縫結果（ユーザー・防具毎）
+  id: number;
+  userId: string;
+  armorId: number;
+  threeStar: number;
+  twoStar: number;
+  oneStar: number;
+  zeroStar: number;
+  mistake: number;
+  total: number;
+  createdAt: Date;
+  updatedAt: Date;
 }
