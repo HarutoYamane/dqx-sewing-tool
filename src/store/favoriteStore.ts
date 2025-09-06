777eae4d407a30337da2be75c53058fd66d7bc98
Yamane Haruto <haru.momo.coco.happy@gmail.com>
2025-09-06 22:00:53 +0900

    addFavoriteとdeleteFavorite関数を追加

--- src/store/favoriteStore.ts
+++ src/store/favoriteStore.ts
@@ -11,6 +11,10 @@ interface FavoriteState {
   error: string | null;
   // 現在ログイン中のユーザー情報のお気に入りリストを取得する Action
   fetchFavorites: () => Promise<void>;
+  // お気に入りリストに追加する Action
+  addFavorite: (armorId: number, createdAt: Date) => Promise<void>;
+  // お気に入りリストから削除する Action
+  deleteFavorite: (favoriteId: number) => Promise<void>;
   // ユーザー情報をクリアする Action
   clearFavorites: () => void;
 }
@@ -40,6 +44,69 @@ export const useFavoriteStore = create<FavoriteState>((set) => ({
       console.error('ユーザーのお気に入りリストの取得に失敗:', error);
       set({
         error: error instanceof Error ? error.message : 'ユーザーのお気に入りリストの取得に失敗しました',
+        isLoading: false,
+      });
+    }
+  },
+
+  addFavorite: async (armorId: number, createdAt: Date) => {
+    try {
+      set({ isLoading: true, error: null });
+
+      // 重複チェック(基本は起こり得ないが、万一のために追加)
+      const currentState = useFavoriteStore.getState();
+      if (currentState.favorites?.some((favorite) => favorite.armorId === armorId)) {
+        set({ isLoading: false });
+        return; // 既に存在する場合は追加しない
+      }
+
+      const res = await fetch('/api/favorite', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ armorId, createdAt }),
+      });
+      if (!res.ok) {
+        const errorData = await res.json();
+        throw new Error(errorData.error || 'ユーザーのお気に入りリストの追加に失敗しました');
+      }
+      // ストアに追加されたお気に入りを追加する
+      const newFavorite = (await res.json()) as Favorite;
+      set((state) => ({
+        favorites: state.favorites ? [...state.favorites, newFavorite] : [newFavorite],
+        isLoading: false,
+      }));
+    } catch (error) {
+      console.error('ユーザーのお気に入りリストの追加に失敗:', error);
+      set({
+        error: error instanceof Error ? error.message : 'ユーザーのお気に入りリストの追加に失敗しました',
+        isLoading: false,
+      });
+    }
+  },
+
+  deleteFavorite: async (favoriteId: number) => {
+    try {
+      set({ isLoading: true, error: null });
+      const res = await fetch(`/api/favorite`, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ favoriteId }),
+      });
+      if (!res.ok) {
+        const errorData = await res.json();
+        throw new Error(errorData.error || 'ユーザーのお気に入りリストの削除に失敗しました');
+      }
+
+      // ストアから削除されたお気に入りを除去する
+      set((state) => ({
+        // お気に入りが空の場合はnullを返す
+        favorites: state.favorites?.filter((favorite) => favorite.id !== favoriteId) || null,
+        isLoading: false,
+      }));
+    } catch (error) {
+      console.error('ユーザーのお気に入りリストの削除に失敗:', error);
+      set({
+        error: error instanceof Error ? error.message : 'ユーザーのお気に入りリストの削除に失敗しました',
         isLoading: false,
       });
     }
