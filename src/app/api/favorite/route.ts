6b980df3fad0494146a2307a06570f19982dae0d
Yamane Haruto <haru.momo.coco.happy@gmail.com>
2025-09-06 21:56:39 +0900

    お気に入りに追加する処理と削除する処理のAPIを追加

--- src/app/api/favorite/route.ts
+++ src/app/api/favorite/route.ts
@@ -33,3 +33,70 @@ export const GET = withAuth(async (request: NextRequest, _, user: UserProfile) =
     return NextResponse.json({ error: 'サーバーエラーが発生しました' }, { status: 500 });
   }
 });
+
+/**
+ * [POST] /api/favorite: ログイン中のユーザーのお気に入りリストに追加
+ */
+export const POST = withAuth(async (request: NextRequest, _, user: UserProfile) => {
+  try {
+    const { armorId, createdAt } = await request.json();
+
+    // お気に入りを追加
+    const createdFavorite = await prisma.favorite.create({
+      data: { armorId, userId: user.id, createdAt },
+    });
+
+    // 追加されたお気に入りをarmor情報と一緒に取得
+    const addFavorite = await prisma.favorite.findUnique({
+      where: { id: createdFavorite.id },
+      include: {
+        armor: {
+          select: {
+            id: true,
+            name: true,
+            imageUrl: true,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(addFavorite);
+  } catch (error) {
+    console.error('お気に入り追加エラー:', error);
+    return NextResponse.json({ error: 'サーバーエラーが発生しました' }, { status: 500 });
+  }
+});
+
+/**
+ * [DELETE] /api/favorite: ログイン中のユーザーのお気に入りリストから削除
+ */
+export const DELETE = withAuth(async (request: NextRequest, _, user: UserProfile) => {
+  try {
+    const { favoriteId } = await request.json();
+
+    // 削除前に削除対象のオブジェクトを取得
+    const favoriteToDelete = await prisma.favorite.findUnique({
+      where: { id: favoriteId, userId: user.id },
+      include: {
+        armor: {
+          select: { id: true, name: true, imageUrl: true },
+        },
+      },
+    });
+    if (!favoriteToDelete) {
+      return NextResponse.json({ error: 'お気に入りが見つかりません' }, { status: 404 });
+    }
+
+    await prisma.favorite.delete({
+      where: {
+        id: favoriteId,
+        userId: user.id, // ユーザーが自分のお気に入りのみ削除できるようにする
+      },
+    });
+    // 削除されたオブジェクトを返す
+    return NextResponse.json(favoriteToDelete);
+  } catch (error) {
+    console.error('お気に入り削除エラー:', error);
+    return NextResponse.json({ error: 'サーバーエラーが発生しました' }, { status: 500 });
+  }
+});
