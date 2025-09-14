31eb99862c88eec02fa976a061b1ed89688c1647
Yamane Haruto <haru.momo.coco.happy@gmail.com>
2025-09-15 00:55:50 +0900

    ゲストユーザーの場合、リザルトを取得せず、ゲストユーザーの場合のFooter表示を調整

--- src/components/channel/channelFooter.tsx
+++ src/components/channel/channelFooter.tsx
@@ -1,12 +1,14 @@
 'use client';
 // React
 import { useEffect } from 'react';
+// Next.js
+import Link from 'next/link';
 // shadcn/ui
 import { Button } from '@/components/ui/button';
 // 自作コンポーネント
 import Error from '@/app/error';
 // アイコン
-import { RotateCcw } from 'lucide-react';
+import { RotateCcw, CircleAlert } from 'lucide-react';
 // Zustandストア
 import { useResultStore } from '@/store/ResultStore';
 
@@ -14,8 +16,8 @@ export default function ChannelFooter({ channelId, isGuest }: { channelId: numbe
   const { result, isLoading, isUpdateLoading, isResetLoading, error, fetchResult, resetResult } = useResultStore();
 
   useEffect(() => {
-    fetchResult(channelId);
-  }, [channelId, fetchResult]);
+    if (!isGuest) fetchResult(channelId); //ゲストユーザーではない場合は裁縫結果を取得
+  }, [channelId, fetchResult, isGuest]);
 
   if (isLoading || isUpdateLoading || isResetLoading) {
     // 読み込み中または更新中またはリセット中
@@ -40,8 +42,15 @@ export default function ChannelFooter({ channelId, isGuest }: { channelId: numbe
   return (
     <footer className="border-b bg-background z-10">
       {isGuest ? (
-        <div className="h-14 flex items-center justify-start bg-gray-500 px-4">
-          <p className="text-sm text-white font-medium">裁縫結果を保存するにはログインが必要です</p>
+        <div className="h-14 flex items-center justify-start px-4">
+          <CircleAlert className="h-4 w-4 mr-2 text-red-500" />
+          <p className="text-sm font-medium">
+            裁縫結果を保存するには
+            <Link href="/login" className="underline underline-offset-2 text-blue-500 hover:text-blue-800">
+              ログイン
+            </Link>
+            が必要です
+          </p>
         </div>
       ) : (
         <div className="h-14 flex items-center gap-4 px-4">
